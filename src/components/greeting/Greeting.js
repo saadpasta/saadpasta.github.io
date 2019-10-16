1ee3883528ff7435f985ce2ce1131426bef622c9
saadpasta <saadpasta70@gmail.com>
2019-10-16 18:19:15 +0500

    add :  media query and responsive

--- src/components/greeting/Greeting.js
+++ src/components/greeting/Greeting.js
@@ -6,8 +6,10 @@ export default function Greeting() {
     <div className="main">
       <div className="greeting-main">
         <div className="greeting-text-div">
+          <div>
           <h1 className="greeting-text">Hi all I'm Saad</h1>
-          <p className="greeting-text-p">I'm a web developer in Karachi with a passion for exploration and continual learning.</p>
+          <p className="greeting-text-p">I'm a Web and Mobile Application developer in Karachi with a passion for exploration and love for AI and ML.</p>
+          </div>
         </div>
         <div className="greeting-image-div">
           <img alt="saad sitting on table" src={require("../../assests/images/manOnTable.svg")}></img>
