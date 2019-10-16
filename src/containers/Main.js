f566db9127e373220543154176c15b738499b99d
saadpasta <saadpasta70@gmail.com>
2019-10-16 14:22:41 +0500

    add  reponsive header

---
+++ src/containers/Main.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Header from "../components/header/Header";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
