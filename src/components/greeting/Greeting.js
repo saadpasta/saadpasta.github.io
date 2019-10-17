e852c27ab87d7347dc847d9c34d931ea7ea8f08a
saadpasta <saadpasta70@gmail.com>
2019-10-17 11:28:04 +0500

    add : Socail Media Icons without href

--- src/components/greeting/Greeting.js
+++ src/components/greeting/Greeting.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Greeting.css";
+import SocialMedia from "../socialMedia/SocialMedia";
 
 export default function Greeting() {
   return (
@@ -7,8 +8,9 @@ export default function Greeting() {
       <div className="greeting-main">
         <div className="greeting-text-div">
           <div>
-          <h1 className="greeting-text">Hi all I'm Saad</h1>
-          <p className="greeting-text-p">I'm a Web and Mobile Application developer in Karachi with a passion for exploration and love for AI and ML.</p>
+            <h1 className="greeting-text">Hi all I'm Saad</h1>
+            <p className="greeting-text-p">I'm a Web and Mobile Application developer in Karachi with a passion for exploration and love for AI and ML.</p>
+            <SocialMedia />
           </div>
         </div>
         <div className="greeting-image-div">
