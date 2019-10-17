26b9dc6dd07e057a2f19d248a8f588240fd7e20a
saadpasta <saadpasta70@gmail.com>
2019-10-17 12:56:58 +0500

    added : new font awesome

--- src/components/socialMedia/SocialMedia.js
+++ src/components/socialMedia/SocialMedia.js
@@ -5,27 +5,27 @@ export default function socialMedia() {
   return (
     <div className="social-media-div">
       <a href="#" className="icon-button github">
-        <i className="fa fa-github"></i>
+        <i className="fab fa-github"></i>
         <span></span>
       </a>
       <a href="#" className="icon-button linkedin">
-        <i className="fa fa-linkedin"></i>
+        <i className="fab fa-linkedin-in"></i>
         <span></span>
       </a>
       <a href="#" className="icon-button google">
-        <i className="fa fa-google"></i>
+        <i className="fab fa-google"></i>
         <span></span>
       </a>
       <a href="#" className="icon-button gitlab">
-        <i className="fa fa-gitlab"></i>
+        <i className="fab fa-gitlab"></i>
         <span></span>
       </a>
       <a href="#" className="icon-button facebook">
-        <i className="fa fa-facebook"></i>
+        <i className="fab fa-facebook-f"></i>
         <span></span>
       </a>
       <a href="#" className="icon-button twitter">
-        <i className="fa fa-twitter"></i>
+        <i className="fab fa-twitter"></i>
         <span></span>
       </a>
     </div>
