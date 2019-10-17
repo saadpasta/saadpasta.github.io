f0e486eb93f657a4e4e9738cb041715813edab29
saadpasta <saadpasta70@gmail.com>
2019-10-17 13:08:20 +0500

    added : software skills

--- src/components/softwareSkills/SoftwareSkill.js
+++ src/components/softwareSkills/SoftwareSkill.js
@@ -6,7 +6,42 @@ export default function SoftwareSkill() {
     <div>
       <div className="software-skills-main-div">
         <ul className="dev-icons">
-         
+          <li className="software-skill-inline">
+            <i className="fab fa-html5"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-css3-alt"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-sass"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-js"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-react"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-node"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-swift"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-npm"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fas fa-database"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fab fa-aws"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i className="fas fa-fire"></i>
+          </li>
+          <li className="software-skill-inline">
+            <i class="fab fa-python"></i>
+          </li>
         </ul>
       </div>
     </div>
