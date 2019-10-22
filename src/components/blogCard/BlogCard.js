d95a678a4159ae96b5ccc2faf55ca36466a3260b
saadpasta <saadpasta70@gmail.com>
2019-10-22 16:33:10 +0500

    added : Blog Card Component

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "./BlogCard.css";
 
-export default function BlogCard() {
+export default function BlogCard({ blog }) {
   return (
     <div>
       <div class="container">
         <div class="square">
-          <img
-            src="https://images.unsplash.com/photo-1504610926078-a1611febcad3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=e1c8fe0c9197d66232511525bfd1cc82&auto=format&fit=crop&w=1100&q=80"
-            alt="blog"
-            className="blog-card-image mask"
-          />
-          <div class="blog-card-title">Is Apple a Design Company?</div>
-          <p className="blog-card-subtitle">
-            Apple is more than a tech company; it became a culture unto itself, a passion of most of people and the birthplace of the world’s most revolutionized products.
-          </p>
+          <img src={blog.image} alt="blog" className="blog-card-image mask" />
+          <div class="blog-card-title">{blog.title}</div>
+          <p className="blog-card-subtitle">{blog.description}</p>
 
           <div>
-            <a href="https://medium.com/@laheshk/is-apple-a-design-company-f5c83514e261" target="_" class="button">
+            <a href={blog.url} target="_" class="button">
               Read More
             </a>
           </div>
--- src/containers/blogs/Blogs.js
+++ src/containers/blogs/Blogs.js
@@ -11,8 +11,22 @@ export default function Blogs() {
       </div>
       <div className="blog-main-div">
         <div className="blog-text-div">
-          <BlogCard />
-          <BlogCard/>
+          <BlogCard
+            blog={{
+              url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+              image: "https://miro.medium.com/max/400/1*pYHONrA_3QM4dgtBSKrYWQ.jpeg",
+              title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+              description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min."
+            }}
+          />
+          <BlogCard
+            blog={{
+              url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+              image: "https://miro.medium.com/max/1280/1*o08b2PW-1PVs94riZuhNMA.png",
+              title: "Why REACT Is The Best?",
+              description: "React is a JavaScript library for building the user interface. It is maintained by Facebook and a community of individual developers and companies."
+            }}
+          />
         </div>
       </div>
     </div>
