00ae5c6291d952d91e198ef50286e60b3f0c0d1a
saadpasta <saadpasta70@gmail.com>
2019-10-28 18:37:32 +0500

    change : header text

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,7 +6,7 @@ function Header() {
     <div>
       <header className="header">
         <a href="" className="logo">
-          Saad Pasta
+          <span className="grey-color"> &lt;</span> Saad Pasta <span className="grey-color">/&gt;</span>
         </a>
         <input className="menu-btn" type="checkbox" id="menu-btn" />
         <label className="menu-icon" htmlFor="menu-btn">
