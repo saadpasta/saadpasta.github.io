964a6f8d7e1aaa110ca1c7a481412deffce51ba7
saadpasta <saadpasta70@gmail.com>
2019-10-30 12:45:07 +0500

    new : build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.35a4cef4cd8f291e3916835b45ce0ab9.js"
+  "/precache-manifest.87dc6201a01c588b133b10ec9ffd0420.js"
 );
 
 self.addEventListener('message', (event) => {
